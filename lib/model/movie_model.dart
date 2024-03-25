9dccd58e7a8897ed1c35604f1b0dedbb8934f50d
Sahil <sahilarneja2003@gmail.com>
2024-03-25 23:14:52 +0530

    updated

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -3,30 +3,52 @@ class Movie {
   final String backDropPath;
   final String overview;
   final String posterPath;
+  final String director;
+  final String actors;
+  final String runtime;
+  final String genre;
+  final String language;
+  final String imdbRating;
 
   Movie({
     required this.title,
     required this.backDropPath,
     required this.overview,
     required this.posterPath,
+    required this.director,
+    required this.actors,
+    required this.runtime,
+    required this.genre,
+    required this.language,
+    required this.imdbRating,
   });
 
-  factory Movie.fromMap(Map<String, dynamic> map) {
-    return Movie(
-      title: map['Title'],
-      backDropPath: map['Poster'],
-      overview: map['Plot'],
-      posterPath: map['Poster'],
-    );
-  }
+factory Movie.fromMap(Map<String, dynamic> map) {
+  return Movie(
+    title: map['Title'] ?? '',
+    backDropPath: map['Poster'] ?? '',
+    overview: map['Plot'] ?? '',
+    posterPath: map['Poster'] ?? '',
+    director: map['Director'] ?? '',
+    actors: map['Actors'] ?? '',
+    runtime: map['Runtime'] ?? '',
+    genre: map['Genre'] ?? '',
+    language: map['Language'] ?? '',
+    imdbRating: map['imdbRating'] ?? '',
+  );
+}
 
   Map<String, dynamic> toMap() {
     return {
       'Title': title,
       'Poster': backDropPath,
       'Plot': overview,
-      // We don't include 'posterPath' here as it's not present in the JSON structure
-      
+      'Director': director,
+      'Actors': actors,
+      'Runtime': runtime,
+      'Genre': genre,
+      'Language': language,
+      'imdbRating': imdbRating,
     };
   }
 }
--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,6 @@
-import 'package:cinemate_app/model/movie_model.dart';
 import 'package:flutter/material.dart';
+import 'package:cinemate_app/model/movie_model.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key, required this.movie}) : super(key: key);
@@ -9,11 +10,91 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black, // Set the background color of the entire screen to black
       appBar: AppBar(
-        title: Text(movie.title),
+        backgroundColor: Colors.black,
+        title: Text(
+          'CineMate',
+          style: GoogleFonts.aBeeZee(
+            color: Color.fromARGB(255, 255, 0, 0),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.white), // Change back arrow color to white
       ),
-      body: Center(
-        child: Text('Movie details will be displayed here.'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Movie Poster
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Image.network(
+                movie.posterPath,
+                width: 200,
+              ),
+            ),
+            // Movie Title
+            Text(
+              movie.title,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // Set text color to white
+              ),
+            ),
+            SizedBox(height: 8),
+            // Movie Overview
+            Text(
+              'Overview:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // Set text color to white
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              movie.overview,
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+            SizedBox(height: 16),
+            // Other Movie Details
+            Text(
+              'Director: ${movie.director}',
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Actors: ${movie.actors}',
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Runtime: ${movie.runtime}',
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Genre: ${movie.genre}',
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Language: ${movie.language}',
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+            SizedBox(height: 8),
+            Text(
+              'IMDb Rating: ${movie.imdbRating}',
+              style: TextStyle(fontSize: 16, color: Colors.white), // Set text color to white
+            ),
+          ],
+        ),
       ),
     );
   }
